317697169b1f5b89b54fa92556167c06d43adeea
Leo <Leobrac23@gmail.com>
2020-02-16 17:06:39 -0300

    Some code cleaning part two

--- src/main/java/com/leobeliik/extremesoundmuffler/utils/EventHandler.java
+++ src/main/java/com/leobeliik/extremesoundmuffler/utils/EventHandler.java
@@ -18,7 +18,7 @@ public final class EventHandler {
 
     private static final Map<BlockPos, Set<ResourceLocation>> sounds = new HashMap<>();
     private static final byte muteIt = 0;
-    private static final String[] forbidenSounds = {"music", "ui.button", "ambient.cave"};
+    private static final String[] forbidenSounds = {"music", "ui.button", "ambient.cave", "ui.toast"};
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void onSoundPlaying(PlaySoundEvent event) {
@@ -26,6 +26,7 @@ public final class EventHandler {
         Set<BlockPos> soundMufflers = SoundMufflerBlock.getPositions();
         if (soundMufflers.isEmpty()) return;
         soundMufflers.forEach(pos -> {
+            if (distance(sound, pos) >= 32) return;
             if (sound instanceof ITickableSound) {
                 event.setResultSound(new Muffler.TickableMuffler((ITickableSound) sound, muteIt));
             } else {
@@ -33,16 +34,14 @@ public final class EventHandler {
                 for (String fs : forbidenSounds) {
                     if (soundLocat.toString().contains(fs)) return;
                 }
-                if (distance(sound, pos) <= 32) {
-                    if (!sounds.containsKey(pos)) {
-                        sounds.put(pos, new HashSet<>());
-                    }
-                    sounds.get(pos).add(soundLocat);
-                    Set<ResourceLocation> mufflerList = SoundMufflerBlock.getMufflerOnPosition(pos);
-                    if (mufflerList == null) return;
-                    if (mufflerList.contains(soundLocat)) {
-                        event.setResultSound(new Muffler(sound, muteIt));
-                    }
+                if (!sounds.containsKey(pos)) {
+                    sounds.put(pos, new HashSet<>());
+                }
+                sounds.get(pos).add(soundLocat);
+                Set<ResourceLocation> mufflerList = SoundMufflerBlock.getMufflerOnPosition(pos);
+                if (mufflerList == null) return;
+                if (mufflerList.contains(soundLocat)) {
+                    event.setResultSound(new Muffler(sound, muteIt));
                 }
             }
         });
@@ -65,8 +64,8 @@ public final class EventHandler {
     private static double distance(ISound sound, BlockPos pos) {
         return Math.sqrt( // d(P1, P2) = √(x2 - x1)² + (y2 - y1)² + (z2 - z1)²'
                 Math.pow((sound.getX() - pos.getX()), 2)
-              + Math.pow((sound.getY() - pos.getY()), 2)
-              + Math.pow((sound.getZ() - pos.getZ()), 2)
+                        + Math.pow((sound.getY() - pos.getY()), 2)
+                        + Math.pow((sound.getZ() - pos.getZ()), 2)
         );
     }
 }
