03e36e5c9dcb7f32e45fd1a4953858acc7167cc7
Leo <Leobrac23@gmail.com>
2020-02-17 17:30:33 -0300

    Beta 1.0

--- src/main/java/com/leobeliik/extremesoundmuffler/utils/EventHandler.java
+++ src/main/java/com/leobeliik/extremesoundmuffler/utils/EventHandler.java
@@ -28,23 +28,22 @@ public final class EventHandler {
         Set<BlockPos> soundMufflers = SoundMufflerBlock.getPositions();
         if (soundMufflers.isEmpty()) return;
         soundMufflers.forEach(pos -> {
-            if (sound instanceof ITickableSound) {
+            if (distance(sound, pos) >= 32) return;
+            if (sound instanceof ITickableSound) { //This will block all constant sounds (like minecarts), dunno if i want this.
                 event.setResultSound(new Muffler.TickableMuffler((ITickableSound) sound));
             } else {
                 ResourceLocation soundLocat = sound.getSoundLocation();
                 for (String fs : forbidenSounds) {
                     if (soundLocat.toString().contains(fs)) return;
                 }
-                if (distance(sound, pos) <= 32) {
-                    if (!sounds.containsKey(pos)) {
-                        sounds.put(pos, new HashSet<>());
-                    }
-                    sounds.get(pos).add(soundLocat);
-                    Set<ResourceLocation> mufflerList = SoundMufflerBlock.getMufflerOnPosition(pos);
-                    if (mufflerList == null) return;
-                    if (mufflerList.contains(soundLocat)) {
-                        event.setResultSound(new Muffler(sound));
-                    }
+                if (!sounds.containsKey(pos)) {
+                    sounds.put(pos, new HashSet<>());
+                }
+                sounds.get(pos).add(soundLocat);
+                Set<ResourceLocation> mufflerList = SoundMufflerBlock.getMufflerOnPosition(pos);
+                if (mufflerList == null) return;
+                if (mufflerList.contains(soundLocat)) {
+                    event.setResultSound(new Muffler(sound));
                 }
             }
         });
@@ -67,8 +66,8 @@ public final class EventHandler {
     private static double distance(ISound sound, BlockPos pos) {
         return Math.sqrt( // d(P1, P2) = √(x2 - x1)² + (y2 - y1)² + (z2 - z1)²'
                 Math.pow((sound.getX() - pos.getX()), 2)
-               + Math.pow((sound.getY() - pos.getY()), 2)
-               + Math.pow((sound.getZ() - pos.getZ()), 2)
+                        + Math.pow((sound.getY() - pos.getY()), 2)
+                        + Math.pow((sound.getZ() - pos.getZ()), 2)
         );
     }
 }
