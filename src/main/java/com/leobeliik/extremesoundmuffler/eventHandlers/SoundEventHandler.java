9871f0243b75b7e9fccb50bd5684aed0c18f2b36
Max Henkel <max@maxhenkel.de>
2021-05-12 21:53:36 +0200

    Fix tickable sounds not working

--- src/main/java/com/leobeliik/extremesoundmuffler/eventHandlers/SoundEventHandler.java
+++ src/main/java/com/leobeliik/extremesoundmuffler/eventHandlers/SoundEventHandler.java
@@ -7,35 +7,24 @@ import com.leobeliik.extremesoundmuffler.interfaces.IAnchorList;
 import com.leobeliik.extremesoundmuffler.interfaces.ISoundLists;
 import com.leobeliik.extremesoundmuffler.utils.Anchor;
 import com.leobeliik.extremesoundmuffler.utils.MuffledSound;
-import com.leobeliik.extremesoundmuffler.utils.MuffledSound.MuffledTickableSound;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.ISound;
 import net.minecraft.client.audio.ITickableSound;
-import net.minecraft.client.audio.SoundEngine;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import java.util.ArrayList;
-import java.util.List;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = SoundMuffler.MODID)
 public class SoundEventHandler implements ISoundLists, IAnchorList {
 
-    //Change probably all of this with mixin
-
     private static boolean isFromPSB = false;
-    private static List<ITickableSound> tickSoundList = new ArrayList<>();
-    private static SoundEngine soundEngine;
 
     @SubscribeEvent
     @OnlyIn(Dist.CLIENT)
     public static void onSoundPlaying(PlaySoundEvent event) {
-
-        soundEngine = event.getManager();
 
         if (Minecraft.getInstance().world == null) {
             return;
@@ -49,32 +38,15 @@ public class SoundEventHandler implements ISoundLists, IAnchorList {
         ISound sound = event.getSound();
         BlockPos soundPos = new BlockPos(sound.getX(), sound.getY(), sound.getZ());
 
-        if (sound instanceof ITickableSound) {
-            boolean contains = false;
-            for (ISound tickSound : tickSoundList) {
-                if (tickSound.getSoundLocation().equals(sound.getSoundLocation())) {
-                    contains = true;
-                    break;
-                }
-            }
-            if (!contains) {
-                tickSoundList.add((ITickableSound) sound);
-            }
-        }
-
-        for (String fs : forbiddenSounds) {
-            if (sound.getSoundLocation().toString().contains(fs)) {
-                return;
-            }
+        if (isForbidden(sound)) {
+            return;
         }
 
         recentSoundsList.add(sound.getSoundLocation());
 
         if (MainScreen.isMuffled()) {
             if (muffledSounds.containsKey(sound.getSoundLocation())) {
-                if (sound instanceof ITickableSound) {
-                    event.setResultSound(new MuffledTickableSound((ITickableSound) sound, muffledSounds.get(sound.getSoundLocation()).floatValue()));
-                } else {
+                if (!(sound instanceof ITickableSound)) {
                     event.setResultSound(new MuffledSound(sound, muffledSounds.get(sound.getSoundLocation()).floatValue()));
                 }
                 return;
@@ -98,17 +70,13 @@ public class SoundEventHandler implements ISoundLists, IAnchorList {
         }
     }
 
-    public static void reloadSound(ResourceLocation sound) {
-        if (soundEngine == null) {
-            return;
-        }
-        for (ITickableSound tickSound : tickSoundList) {
-            if (tickSound.getSoundLocation().equals(sound)) {
-                soundEngine.stop(sound, tickSound.getCategory());
-                soundEngine.play(tickSound);
-                break;
+    public static boolean isForbidden(ISound sound) {
+        for (String fs : forbiddenSounds) {
+            if (sound.getSoundLocation().toString().contains(fs)) {
+                return true;
             }
         }
+        return false;
     }
 
     public static void isFromPlaySoundButton(boolean b) {
