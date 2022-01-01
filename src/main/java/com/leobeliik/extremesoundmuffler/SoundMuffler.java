5fba4f9418d1bd6089a0d5277470ea632bb047c9
LeoBeliik <Leobrac23@gmail.com>
2022-01-01 18:51:59 -0300

    Data Loader is now Mixin

--- src/main/java/com/leobeliik/extremesoundmuffler/SoundMuffler.java
+++ src/main/java/com/leobeliik/extremesoundmuffler/SoundMuffler.java
@@ -1,23 +1,17 @@
 package com.leobeliik.extremesoundmuffler;
 
 import com.leobeliik.extremesoundmuffler.gui.MainScreen;
-import com.leobeliik.extremesoundmuffler.gui.buttons.InvButton;
 import com.leobeliik.extremesoundmuffler.interfaces.ISoundLists;
 import com.leobeliik.extremesoundmuffler.utils.DataManager;
 import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
-import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.client.event.ScreenEvent;
-import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -26,15 +20,12 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod("extremesoundmuffler")
 public class SoundMuffler {
 
     static final String MODID = "extremesoundmuffler";
     private static KeyMapping openMufflerScreen;
-    private static final Logger LOGGER = LogManager.getLogger();
 
     public SoundMuffler() {
         MinecraftForge.EVENT_BUS.register(this);
@@ -54,12 +45,6 @@ public class SoundMuffler {
                 InputConstants.UNKNOWN,
                 "key.categories.misc");
         ClientRegistry.registerKeyBinding(openMufflerScreen);
-    }
-
-    @SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
-    public void onPlayerLoggin(ClientPlayerNetworkEvent.LoggedInEvent event) {
-        DataManager.loadData();
     }
 
     @SubscribeEvent
