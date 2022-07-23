2ff0d418ced666f0421b4d7780d3a5626051bd93
Leo <Leobrac23@gmail.com>
2022-07-23 07:13:28 -0300

    Doing a better job, I hope.

--- Common/src/main/java/com/leobeliik/extremesoundmuffler/utils/DataManager.java
+++ Common/src/main/java/com/leobeliik/extremesoundmuffler/utils/DataManager.java
@@ -7,12 +7,15 @@ import com.google.gson.stream.JsonReader;
 import com.leobeliik.extremesoundmuffler.CommonConfig;
 import com.leobeliik.extremesoundmuffler.Constants;
 import com.leobeliik.extremesoundmuffler.interfaces.ISoundLists;
+import net.minecraft.FileUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -47,7 +50,11 @@ public class DataManager implements ISoundLists {
             name = Minecraft.getInstance().getSingleplayerServer().getWorldData().getLevelName();
         }
         //prevent to create a directory with reserved characters
-        return name.replaceAll("[^a-zA-Z0-9\s]", "_");
+        try {
+            return FileUtil.findAvailableName(Path.of(""), name, "");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static CompoundTag serializeAnchor(Anchor anchor) {
@@ -96,7 +103,7 @@ public class DataManager implements ISoundLists {
         try (Writer writer = new OutputStreamWriter(new FileOutputStream("ESM/soundsMuffled.dat"), StandardCharsets.UTF_8)) {
             writer.write(gson.toJson(muffledSounds));
         } catch (IOException e) {
-            Constants.LOG.error("ESM: Error saving the muffled sounds list\n" + e);
+            Constants.LOG.error(Component.translatable("log.error.saveMuffledList", e).getString());
         }
     }
 
@@ -105,7 +112,11 @@ public class DataManager implements ISoundLists {
             return gson.fromJson(new JsonReader(reader), new TypeToken<Map<String, Double>>() {
             }.getType());
         } catch (Exception e) {
-            Constants.LOG.error("ESM: Error loading the muffled sounds list\n" + e);
+            if (e instanceof FileNotFoundException) {
+                Constants.LOG.warn(Component.translatable("log.warn.loadMuffledList").getString());
+            } else {
+                Constants.LOG.error(Component.translatable("log.error.loadMuffledList", e).getString());
+            }
             return new HashMap<>();
         }
     }
@@ -115,7 +126,7 @@ public class DataManager implements ISoundLists {
         try (Writer writer = new OutputStreamWriter(new FileOutputStream("ESM/" + getWorldName() + "/anchors.dat"), StandardCharsets.UTF_8)) {
             writer.write(gson.toJson(anchorList));
         } catch (IOException e) {
-            Constants.LOG.error("ESM: Error saving the Anchor list\n" + e);
+            Constants.LOG.error(Component.translatable("log.error.saveAnchorList", e).getString());
         }
     }
 
@@ -123,7 +134,11 @@ public class DataManager implements ISoundLists {
         try (InputStreamReader reader = new InputStreamReader(new FileInputStream("ESM/" + getWorldName() + "/anchors.dat"), StandardCharsets.UTF_8)) {
             return gson.fromJson(new JsonReader(reader), new TypeToken<List<Anchor>>() {}.getType());
         } catch (Exception e) {
-            Constants.LOG.error(e instanceof FileNotFoundException ? "ESM: Anchor list not found; Creating empty Anchor list" : "ESM: Error loading Anchor list\n" + e);
+            if (e instanceof FileNotFoundException) {
+                Constants.LOG.warn(Component.translatable("log.warn.loadAnchorList").getString());
+            } else {
+                Constants.LOG.error(Component.translatable("log.error.loadAnchorList", e).getString());
+            }
             return IntStream.range(0, 10).mapToObj(i -> new Anchor(i, "Anchor " + i)).collect(Collectors.toList());
         }
     }
