198b06e6a07c774e4a28b53614a2377ebf93b11f
LeoBeliik <11804768+LeoBeliik@users.noreply.github.com>
2026-01-16 14:05:16 -0300

    Update NeoForgeConfig.java

--- neoforge/src/main/java/com/leobeliik/extremesoundmuffler/NeoForgeConfig.java
+++ neoforge/src/main/java/com/leobeliik/extremesoundmuffler/NeoForgeConfig.java
@@ -58,48 +58,91 @@ class NeoForgeConfig {
         String CATEGORY_ANCHORS = "Anchors";
 
         CLIENT_BUILDER.comment("General settings").push(CATEGORY_GENERAL);
-        forbiddenSounds = CLIENT_BUILDER.comment("Blacklisted Sounds - add the name of the sounds to blacklist, separated with comma")
+        
+        forbiddenSounds = CLIENT_BUILDER
+                .comment("Blacklisted Sounds - add the name of the sounds to blacklist, separated with comma")
+                .comment("Default: \"ui.\", \"music.\", \"ambient.\"")
                 .defineList("forbiddenSounds", Arrays.asList("ui.", "music.", "ambient."), () -> "",  o -> o instanceof String);
-        modsMuffled = CLIENT_BUILDER.comment("General mod muffling, any sound from these mods will be muffled down to the provided volume. \n" +
+        
+        modsMuffled = CLIENT_BUILDER
+                .comment("General mod muffling, any sound from these mods will be muffled down to the provided volume. \n" +
                         "Name of the mod and desired volume, separated by \":\" \nExample: \"minecraft:50\", \"extremesoundmuffler:0\"")
+                .comment("Default: Empty")
                 .defineList("modsMuffled", new ArrayList<>(), () -> "", o -> o instanceof String);
-        lawfulAllList = CLIENT_BUILDER.comment("Allow the \"ALL\" sounds list to include the blacklisted sounds?")
+        
+        lawfulAllList = CLIENT_BUILDER
+                .comment("Allow the \"ALL\" sounds list to include the blacklisted sounds?")
+                .comment("Default: false")
                 .define("lawfulAllList", false);
-        defaultMuteVolume = CLIENT_BUILDER.comment("Volume set when pressed the mute button by default")
+        
+        defaultMuteVolume = CLIENT_BUILDER
+                .comment("Volume set when pressed the mute button by default")
+                .comment("Default: 0")            
                 .defineInRange("defaultMuteVolume", 0, 0, 0.9);
-        leftButtons = CLIENT_BUILDER.comment("Set to true to move the muffle and play buttons to the left side of the GUI")
+        
+        leftButtons = CLIENT_BUILDER
+                .comment("Set to true to move the muffle and play buttons to the left side of the GUI")
+                .comment("Default: false")
                 .define("leftButtons", false);
-        showTip = CLIENT_BUILDER.comment("Show tips in the Muffler screen?")
+        
+        showTip = CLIENT_BUILDER
+                .comment("Show tips in the Muffler screen?")
+                .comment("Default: true")
                 .define("showTip", true);
-        useDarkTheme = CLIENT_BUILDER.comment("Whether or not use the dark theme")
+        
+        useDarkTheme = CLIENT_BUILDER
+                .comment("Whether or not use the dark theme")
+                .comment("Default: false")
                 .define("useDarkTheme", false);
         CLIENT_BUILDER.pop();
 
         CLIENT_BUILDER.comment("Inventory button settings").push(CATEGORY_INVENTORY_BUTTON);
 
-        disableInventoryButton = CLIENT_BUILDER.comment("Disable the Muffle button in the player inventory?")
+        disableInventoryButton = CLIENT_BUILDER
+                .comment("Disable the Muffle button in the player inventory?")
+                .comment("Default: false")
                 .define("disableInventoryButton", false);
-        invButtonHorizontal = CLIENT_BUILDER.comment("Coordinates for the Muffler button in the player inventory.\n " +
+        
+        invButtonHorizontal = CLIENT_BUILDER
+                .comment("Coordinates for the Muffler button in the player inventory.\n " +
                         "You can change this in game by holding the RMB over the button and draging it around")
+                .comment("Default: 75")
                 .defineInRange("invButtonX", 75, Integer.MIN_VALUE, Integer.MAX_VALUE);
-        invButtonVertical = CLIENT_BUILDER.comment("Coordinates for the Muffler button in the player inventory. \n" +
+        
+        invButtonVertical = CLIENT_BUILDER
+            .comment("Coordinates for the Muffler button in the player inventory. \n" +
                         "You can change this in game by holding the RMB over the button and draging it around")
+                .comment("Default: 7")
                 .defineInRange("invButtonY", 7, Integer.MIN_VALUE, Integer.MAX_VALUE);
-        disableCreativeInventoryButton = CLIENT_BUILDER.comment("Disable the Muffle button in the creative player inventory?")
+        
+        disableCreativeInventoryButton = CLIENT_BUILDER
+                .comment("Disable the Muffle button in the creative player inventory?")
+                .comment("Default: false")
                 .define("disableCreativeInventoryButton", false);
-        creativeInvButtonHorizontal = CLIENT_BUILDER.comment("Coordinates for the Muffler button in the creative player inventory.\n " +
+        
+        creativeInvButtonHorizontal = CLIENT_BUILDER
+                .comment("Coordinates for the Muffler button in the creative player inventory.\n " +
                         "You can change this in game by holding the RMB over the button and draging it around")
+                .comment("Default: 2")
                 .defineInRange("creativeInvButtonX", 2, Integer.MIN_VALUE, Integer.MAX_VALUE);
-        creativeInvButtonVertical = CLIENT_BUILDER.comment("Coordinates for the Muffler button in the creative player inventory. \n" +
+        
+        creativeInvButtonVertical = CLIENT_BUILDER
+                .comment("Coordinates for the Muffler button in the creative player inventory. \n" +
                         "You can change this in game by holding the RMB over the button and draging it around")
+                .comment("Default: 2")
                 .defineInRange("creativeInvButtonY", 2, Integer.MIN_VALUE, Integer.MAX_VALUE);
         CLIENT_BUILDER.pop();
 
         CLIENT_BUILDER.comment("Anchor settings").push(CATEGORY_ANCHORS);
 
-        disableAnchors = CLIENT_BUILDER.comment("Disable the Anchors?")
+        disableAnchors = CLIENT_BUILDER
+                .comment("Disable the Anchors?")
+                .comment("Default: false")
                 .define("disableAnchors", false);
-        maxAnchorRange = CLIENT_BUILDER.comment("Set max size for anchors (Warning: high values may cause LAG!).")
+        
+        maxAnchorRange = CLIENT_BUILDER
+                .comment("Set max size for anchors (Warning: high values may cause LAG!).")
+                .comment("Default: 32")
                 .defineInRange("anchorRange", 32, 1, Integer.MAX_VALUE);
 
         CLIENT_BUILDER.pop();
